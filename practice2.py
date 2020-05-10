0f83d1fca94a7cddbd1662fcb4f6ce62c7286c02
Id Avila <i.avilacamargo@ugto.mx>
2020-05-09 23:03:18 -0500

    new adds on main practice.py, tests in others docs

--- practice2.py
+++ practice2.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  6 02:34:23 2020
+Created on Fri May  8 17:28:31 2020
 
-@author: IdAvila
+@author: harpn
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import math as mt
+import collections as col
 
 def quartile(lista, percentile):
     q = (len(lista)-1)*percentile
@@ -46,7 +47,7 @@ i_f = w_d+'survey_results_public.csv'
 
 data = pd.read_csv(i_f, encoding = 'utf-8')
 
-column = 'ConvertedComp'
+column = 'Ethnicity'
 values = data[column].tolist()
 n = len(values)
 
@@ -56,28 +57,24 @@ woman = 0
 list_man = []
 list_woman = []
 
-for index, row in data.iterrows():
-    cost = row[column]
-    
-    if not mt.isnan(cost):
-        if cost == 0:
-            continue
-        else:
-            if (row['Gender'] == 'Man'):
-                list_man.append(cost)
-                man += 1
-            elif (row['Gender'] == 'Woman'):
-                list_woman.append(cost)
-                woman += 1
-    
-print('Man salaries: ')
-fiveNumbers(list_man)
-print("Count: " + str(man))
-print('\nWoman salaries:')
-fiveNumbers(list_woman)
-print("Count: " + str(woman))
 
-fig, axes = plt.subplots(nrows=1, ncols=2)
-axes[0].boxplot(list_man)
-axes[1].boxplot(list_woman)
-plt.show()
+
+datos = dict(col.Counter(values)) 
+d_datos = datos.keys()
+dates = d_datos.tolist()
+
+#for index, row in data.iterrows():
+#    if row['ConvertedComp']:
+#        survived += 1
+#        
+#        if row['Sex'] == 'male':
+#            mens_s += 1
+#        else:
+#            womens_s += 1
+#    else:
+#        deaths += 1
+#        
+#        if row['Sex'] == 'male':
+#            mens_d += 1
+#        else:
+#            womens_d += 1
---
+++ practice3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 19:07:50 2020
+
+@author: harpn
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math as mt
+import collections as col
+import operator as op
+
+w_d = 'C:/Users/harpn/OneDrive - Universidad de Guanajuato/Mineria/practice/'
+i_f = w_d+'survey_results_public.csv'
+
+data = pd.read_csv(i_f, encoding = 'utf-8')
+values = data['Country']
+
+datos = dict(col.Counter(values)) 
+maxi = max(datos.items(), key=op.itemgetter(1))[0]
+
+plt.bar(range(len(datos)), datos.values(), align='center')
+plt.xticks(range(len(datos)), list(datos.keys()))
+
+plt.show()
